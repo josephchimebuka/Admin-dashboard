6bd1f49c135c66f870ff4830f162aaa8bfc18268
josephchimebuka <chimebujoseph@gmail.com>
2024-04-15 14:20:16 +0000

    complete install

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import {BrowserRouter} from 'react-router-dom'
+function App() {
+  return (
+    <div className="App">
+     <h1>Dashboard app build </h1>
+     <h1>fresh</h1>
+     <BrowserRouter></BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
